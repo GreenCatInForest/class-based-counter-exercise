8ba6c08e134d4d28331a78d1bd89786a6417d62d
GreenCatInForest <event2016.2017@gmail.com>
2023-06-21 16:52:56 +0100

    create components

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Counter</div>;
+  }
+}
