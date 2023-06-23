a3a77f317e3009eb3e8d24fefda9f68110a81286
GreenCatInForest <event2016.2017@gmail.com>
2023-06-23 15:44:00 +0100

    add class based counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,16 +3,35 @@ import { Component } from "react";
 export class Counter extends Component {
   constructor(props) {
     super(props);
+    this.state = { count: 0 };
   }
+
+  handleDecrement = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  };
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleReset = () => {
+    this.setState({ count: 0 });
+  };
 
   render() {
     return (
       <div className="border m-3 p-4 rounded">
-        <div className="m-3 text-center">Counter:0</div>
+        <div className="m-3 text-center">Counter: {this.state.count}</div>
         <div className="m-1 d-grid gap-2 col-6 mx-auto">
-          <button className="btn btn-danger">Decrement</button>
-          <button className="btn btn-secondary">Reset</button>
-          <button className="btn btn-success">Increment</button>
+          <button className="btn btn-danger" onClick={this.handleDecrement}>
+            Decrement
+          </button>
+          <button className="btn btn-secondary" onClick={this.handleReset}>
+            Reset
+          </button>
+          <button className="btn btn-success" onClick={this.handleIncrement}>
+            Increment
+          </button>
         </div>
       </div>
     );
